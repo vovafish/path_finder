89446cd800579a5cb86929ece70345da209fce68
Vladimir <vladimirrybakov123@gmail.com>
2024-06-18 21:32:52 +0100

    imported all the packages that are required for the project. and tested that stdscr is working

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-#  start
+import curses
+from curses import wrapper
+import queue
+import time
+
+maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
+
+
+def main(stdscr):  # standard output screen
+    stdscr.clear()
+    stdscr.addstr(0, 0, "hi world")
+    stdscr.refresh()
+    stdscr.getch()  # get character
+
+
+wrapper(main)
